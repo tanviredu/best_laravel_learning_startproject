3dd471d42363c23e30d73a5539d97b15893fdb81
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-18 23:55:43 +0600

    added

---
+++ pluralsight-laravel-getting-started/database/seeds/PostTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $post = new  \App\Post([
+            'title' => 'learning Laravel',
+            'content' =>'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
+        ]);
+        $post->save();
+
+        $post = new  \App\Post([
+            'title' => 'learning Laravel Second',
+            'content' =>' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            '
+        ]);
+        $post->save();
+    }
+}
